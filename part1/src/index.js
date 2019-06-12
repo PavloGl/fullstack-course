e376bbabc17c4931ec23b2244e446b520ca38a03
Pavlo Glushko <pawel.glushko@gmail.com>
2019-06-12 19:34:36 +0200

    step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,13 +7,21 @@ const Header = ({course}) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+       {props.part} {props.exercise}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part} {props.exercise}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
   )
 }
 
@@ -38,9 +46,9 @@ const App = () => {
     <div>
       <Header course={course} />
 
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part1={part1} exercise1={exercises1}
+        part2={part2} exercise2={exercises2}
+        part3={part3} exercise3={exercises3} />
 
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
