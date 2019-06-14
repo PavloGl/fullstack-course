7ecfa2cc57aea3436a27c2f07fe037499e8230c2
Pavlo Glushko <pawel.glushko@gmail.com>
2019-06-14 15:46:15 +0200

    unicafe step1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,64 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = ({course}) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
+const FeedbackHeader = () => <h1>give feedback</h1>
 
-const Part = (props) => {
-  return (
-    <p>
-       {props.part} {props.exercise}
-    </p>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const Content = ({parts}) => {
-  return (
-    <div>
-      { parts.map(item => <Part part={item.name} exercise={item.exercises} /> )}
-    </div>
-  )
-}
+const StatisticHeader = () => <h1>statistics</h1>
 
-const Total = ({parts}) => {
-  return (
-    <>
-      <p>Number of exercises {parts.reduce((a,b) => { return a + b.exercises }, 0)}</p>
-    </>
-  )
-}
+const ShowResult = (props) => <div>{props.text} {props.good}</div>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-
-      <Content parts={course.parts} />
-
-      <Total parts={course.parts} />
+      <FeedbackHeader />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <StatisticHeader />
+      <ShowResult text={"good"} good={good} />
+      <ShowResult text={"neutral"} good={neutral} />
+      <ShowResult text={"bad"} good={bad} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
