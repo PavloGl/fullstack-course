8c5a7c0eecc08b099ca94d2da27134d5a52722b2
Pavlo Glushko <pawel.glushko@gmail.com>
2019-07-27 20:39:55 +0200

    Phonebook backend step3

--- part3/phonebook-backend/src/server.js
+++ part3/phonebook-backend/src/server.js
@@ -9,6 +9,13 @@ app.get('/api/persons', (req, res) => {
   res.json(notes)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  note = notes.persons.filter(n => n.id === id)
+  if (note.length === 1) res.json(note)
+  res.status(404).end()
+})
+
 app.get('/info', (req, res) => {
   res.send(`Phonebook has info for ${countPersons()} people \n${new Date()}`)
 })
